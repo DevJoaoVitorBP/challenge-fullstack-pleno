e5ff74de0af3342539b60273152a43d77d2b999b
DevJoaoVitorBP <joaov.batalha013@hotmail.com>
2026-06-22 15:37:36 -0300

    feat(events): implementação de eventos (OrderCreated, ProductCreated, StockLow)

---
+++ app/Events/ProductCreated.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Product;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class ProductCreated
+{
+    use Dispatchable, SerializesModels;
+
+    public function __construct(
+        public Product $product
+    ) {}
+}
