4aa72d2269f8cdc1734446d4498b4ba0b048bb62
DevJoaoVitorBP <joaov.batalha013@hotmail.com>
2026-06-23 15:48:31 -0300

    BugFix: corrigindo teste.

--- tests/Feature/OrderControllerTest.php
+++ tests/Feature/OrderControllerTest.php
@@ -7,6 +7,7 @@ use App\Models\Order;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Bus;
 use Tests\TestCase;
 
 class OrderControllerTest extends TestCase
@@ -67,8 +68,12 @@ class OrderControllerTest extends TestCase
 
     public function test_user_can_create_order_from_cart(): void
     {
-        // Add item to cart
+
+        Bus::fake();
+
+        // Adicionando um item ao carrinho do usuário
         $cart = Cart::factory()->create(['user_id' => $this->user->id]);
+        
         $cart->items()->create([
             'product_id' => $this->product->id,
             'quantity' => 2,
